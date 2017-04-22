9bb63a88e58153a5d06852430c4dd87ddf0a40cb
Swynfel <swynfel@gmail.com>
2017-04-22 16:51:14 +0200

    Setup project

---
+++ Hopeless Void/Assets/Scripts/HexagonBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Building {
+	NONE,
+	CAPITAL,
+	FARM,
+	MINE
+};
+
+public class HexagonBehavior : MonoBehaviour {
+
+	public Sprite[] plains;
+	public Sprite[] mountains;
+
+	public SpriteRenderer renderer;
+	public bool isflat;
+	public 
+
+	void Awake() {
+		Sprite[] sprites;
+		if (isflat) {
+			sprites = plains;
+		} else {
+			sprites = mountains;
+		}
+		renderer.sprite = sprites[Random.Range (0, sprites.Length)];
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
