2c23211e17b3001a774e5ce26b23e890b4afd917
Hédi Sellami <cydouzo@gmail.com>
2017-04-22 15:28:47 +0200

    Initial Commit

---
+++ Hopeless Void/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+	static private GameHandler m_Instance;
+	static public GameHandler instance { get { return m_Instance; } }
+
+	public State gameState;
+	public State menuState;
+	public SoundHandler soundHandler;
+
+	void Awake(){
+		if (m_Instance != null) {
+			Destroy (this);
+		} else {
+			m_Instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		soundHandler.playMusic ("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
---
+++ Hopeless Void/Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour {
+
+	// Use this for initialization
+	public Dictionary<string,AudioClip> musics = new Dictionary<string,AudioClip>();
+	public Dictionary<string,AudioClip> sounds = new Dictionary<string,AudioClip>();
+	public AudioClip[] musiques;
+	public AudioClip[] sons;
+	public AudioSource musicPlayer;
+
+	private const int NUMBER_SOUNDS = 3;
+	private int currentSound = 0;
+	private AudioSource[] soundPlayers = new AudioSource[NUMBER_SOUNDS] ;
+
+	void Start () {
+		foreach(AudioClip audioClip in musiques){
+			musics.Add (audioClip.name, audioClip);
+		}
+		foreach(AudioClip audioClip in sons){
+			sounds.Add (audioClip.name, audioClip);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playMusic (string name){
+		musicPlayer.clip = musics[name];
+		musicPlayer.Play ();
+	}
+
+	public void playSound (string name){
+		soundPlayers [currentSound].PlayOneShot (sounds[name]);
+		currentSound = (currentSound + 1) % NUMBER_SOUNDS;
+	}
+}
