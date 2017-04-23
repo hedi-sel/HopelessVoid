f86231502d40438d67dbd7c419ccd9d79b3ea6fd
Swynfel <swynfel@gmail.com>
2017-04-23 04:40:40 +0200

    Added graphics

---
+++ Hopeless Void/Assets/Scripts/ActionPanelBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionPanelBehavior : MonoBehaviour {
+
+	public Text textName;
+	public Text textAction;
+	public Text textFraction;
+	public Image imageBackground;
+	public RectTransform transformBar;
+
+	[System.Serializable]
+	public struct ActionPanel {
+		public string name;
+		public string action;
+		public int numerator;
+		public int denumerator;
+		public Sprite background;
+	}
+
+	public ActionPanel actionPanel;
+
+	void Awake(){
+		if (actionPanel.name!="") {
+			SetActionPanel (actionPanel);
+		}
+	}
+
+	public void SetActionPanel(ActionPanel _actionPanel){
+		actionPanel = _actionPanel;
+		textName.text = _actionPanel.name;
+		textAction.text = _actionPanel.action;
+		textFraction.text = _actionPanel.numerator + "/" + _actionPanel.denumerator;
+		imageBackground.sprite = _actionPanel.background;
+		transformBar.anchorMax = new Vector2(((float) _actionPanel.numerator)/ _actionPanel.denumerator,1);
+		transformBar.offsetMax = Vector2.zero;
+	}
+}
