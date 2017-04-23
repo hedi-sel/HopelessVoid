26248d9ed0f7ab0186e30502e8af43b1d6e9ec2b
Swynfel <swynfel@gmail.com>
2017-04-23 21:30:05 +0200

    Added people flavor

---
+++ Hopeless Void/Assets/Scripts/PopulationOnHexagonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationOnHexagonBehavior : MonoBehaviour {
+
+	public GameObject humain;
+
+	private const float RAYON = 1f;
+	private int people;
+	private List<GameObject> peoples = new List<GameObject>();
+
+	public void SetPeople(int i){
+		while (people < i) {
+			people++;
+			GameObject g = Instantiate (humain, transform);
+			g.transform.localPosition = new Vector3 (Random.Range (-RAYON, RAYON), Random.Range (-RAYON, RAYON), -5f);
+			peoples.Add (g);
+		}
+
+		while (people > i) {
+			people--;
+			GameObject g = peoples [0];
+			peoples.RemoveAt (0);
+			Destroy (g);
+		}
+	}
+}
